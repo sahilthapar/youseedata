1e2bfb5532f3419b7a3136cf086ea7614c309e83
sahilthapar <sahilthapar1992@gmail.com>
2016-09-09 18:28:15 -0400

    Added json2csv conversion and csv files

---
+++ json2csv.js
@@ -0,0 +1,19 @@
+const json2csv = require('json2csv');
+const fs = require('fs');
+const R = require('ramda');
+const audioFeatures = require('./audio_features');
+const songAttr = require('./song_attr');
+const fields1 = R.keys(audioFeatures[0]);
+const fields2 = R.keys(songAttr[0]);
+
+const csv1 = json2csv({ data: audioFeatures, fields: fields1 });
+const csv2 = json2csv({ data: songAttr, fields: fields2 });
+
+fs.writeFile('audio_features.csv', csv1, function(err) {
+  if (err) throw err;
+  console.log('file saved');
+});
+fs.writeFile('song_attr.csv', csv2, function(err) {
+  if (err) throw err;
+  console.log('file saved');
+});
