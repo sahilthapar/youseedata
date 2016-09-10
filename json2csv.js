53f1ac684cb570f201a90889223a25f0a9d2d90e
nitmat <nitinmat@hotmail.com>
2016-09-09 20:53:55 -0400

    Chanhged Write to Append

--- json2csv.js
+++ json2csv.js
@@ -22,7 +22,7 @@ const R = require('ramda');
 module.exports = function(filepath, data){
   const fields  = R.keys(data[0]);
   const csv = json2csv({ data: data, fields: fields });
-  return fs.writeFile(filepath, csv, function(err) {
+  return fs.appendFile(filepath, csv, function(err) {
     if (err) throw err;
     console.log('file saved');
   });
